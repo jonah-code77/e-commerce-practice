efa458586062f7d613da152e93d9710321d6fc6f
jonah <jonahosok@gmail.com>
2025-01-11 10:07:33 +0100

    delete product from the page when delete link is click

--- script/checkout.js
+++ script/checkout.js
@@ -14,7 +14,8 @@ cart.forEach((carItem)=>{
  });
 cartSummaryHtml +=
   `
-    <div class="cart-item-container">
+    <div class="cart-item-container
+    js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
         Delivery date: Tuesday, June 21
     </div>
@@ -102,6 +103,7 @@ document.querySelector('.js-order-summary')
         link.addEventListener('click',()=>{
           const productId = link.dataset.productId;
           removeFromCart(productId);
-         
+          const container = document.querySelector(`.js-cart-item-container-${productId}`);
+         container.remove();
         });
     }));
